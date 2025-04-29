90393d730f491f4986fc0f734e321e5eebf7eeb7
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-04-29 12:40:29 +0530

    modified on real time

--- modules/amplify/main.tf
+++ modules/amplify/main.tf
@@ -11,6 +11,12 @@ resource "aws_amplify_app" "this" {
   custom_rule {
     source = "/<*>"
     target = "/index.html"
+    status = "404-200"
+  }
+
+  custom_rule {
+    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
+    target = "/index.html"
     status = "200"
   }
 
