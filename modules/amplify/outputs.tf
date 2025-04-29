9f4d79e5ebccc5cfe10bc075f8e8bd5878ff3557
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-04-29 20:00:27 +0530

    modified on real time

--- modules/amplify/outputs.tf
+++ modules/amplify/outputs.tf
@@ -1,11 +1,11 @@
 output "app_id" {
-  value = aws_amplify_app.this.id
+  value = aws_amplify_app.this[0].id
 }
 
 output "app_name" {
-  value = aws_amplify_app.this.name
+  value = aws_amplify_app.this[0].name
 }
 
 output "app_url" {
-  value = aws_amplify_app.this.default_domain
+  value = aws_amplify_app.this[0].default_domain
 }
