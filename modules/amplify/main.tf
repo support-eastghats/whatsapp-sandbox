493f23dee958e3b3379e2340821c3389aef13397
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-04-29 13:45:11 +0530

    modified on real time

--- modules/amplify/main.tf
+++ modules/amplify/main.tf
@@ -1,33 +1,8 @@
-resource "aws_amplify_app" "this" {
-  name        = var.app_name
-  repository  = var.repo_url
-  platform    = "WEB"
-  oauth_token = var.github_token
-
-  environment_variables = var.environment_variables
-
-  build_spec = file("${path.module}/buildspec.yml")
-
-  custom_rule {
-    source = "/<*>"
-    target = "/index.html"
-    status = "404-200"
-  }
-
-  custom_rule {
-    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
-    target = "/index.html"
-    status = "200"
-  }
-
-  tags = var.tags
-}
-
 resource "aws_amplify_branch" "main_branch" {
   app_id            = aws_amplify_app.this.id
   branch_name       = var.branch_name
-  stage             = "PRODUCTION"   # ✅ Very Important!
-  framework         = "React"         # ✅ Very Important!
+  stage             = var.stage
+  framework         = "React"
   enable_auto_build = true
 }
 
@@ -42,4 +17,33 @@ resource "aws_amplify_domain_association" "domain" {
   }
 
   depends_on = [aws_amplify_branch.main_branch]
+}
+
+resource "aws_amplify_app" "this" {
+  name        = var.app_name
+  repository  = var.repo_url
+  platform    = "WEB"
+  oauth_token = var.github_token
+
+  environment_variables = var.environment_variables
+  build_spec            = file("${path.module}/buildspec.yml")
+
+  custom_rule {
+    source = "/<*>"
+    target = "/index.html"
+    status = "404-200"
+  }
+
+  custom_rule {
+    source = "</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>"
+    target = "/index.html"
+    status = "200"
+  }
+
+  tags = var.tags
+
+  depends_on = [
+    aws_amplify_branch.main_branch,
+    aws_amplify_domain_association.domain
+  ]
 }
