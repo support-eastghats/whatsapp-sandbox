1fcafd71f493252bfba8b003102c70efb277a41e
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-06-29 03:51:12 +0530

    Initial commit - 4

--- modules/lambda-layer/main.tf
+++ modules/lambda-layer/main.tf
@@ -3,5 +3,5 @@ resource "aws_lambda_layer_version" "shared" {
   compatible_runtimes = ["nodejs18.x"]
   s3_bucket           = "eastghatscxllp-whatsapp-sandbox-shared-lambda-layer"
   s3_key              = "layers/shared-lambda-layer.zip"
-  source_code_hash    = filebase64sha256("path/to/shared-lambda-layer.zip")
+  source_code_hash    = filebase64sha256("${path.module}/shared-lambda-layer.zip")
 }
