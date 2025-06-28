7e4e9e5c5218fa5f42362b1fa3d8498199f1f362
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-06-29 03:37:09 +0530

    Initial commit - 2

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eastghatscxllp-whatsapp-sandbox-tf-state"     # your backend bucket name
     key            = "dev/terraform.tfstate"              # path inside the bucket
-    region         = "ap-south-1"
+    region         = "eu-north-1"
     encrypt        = true
   }
 }
